fcf424c2dd12ce12d31cddff348b3ecec0fc7251
cokepizza <cokepizza3@gmail.com>
2020-04-14 02:23:10 +0900

    [frontend] 기본적인 webpack 설정 추가

--- src/App.js
+++
@@ -1,6 +0,0 @@
-const rootDiv = document.querySelector('root');
-
-window.onpopstate = () => {
-    rootDiv.innerHTML = window.location.pathname;
-    // rootDiv.innerHTML = routes[window.location.pathname];
-}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import Router from './Router';
+
+// const router = new Router();
+console.log('router');
+alert('hahah');
+
+// const rootDiv = document.querySelector('root');
+
+// window.onpopstate = () => {
+//     rootDiv.innerHTML = window.location.pathname;
+//     // rootDiv.innerHTML = routes[window.location.pathname];
+// }
